7fe4181dd8a1e5daeb51c946c84d859b22a92f31
Harmish Khambhaita <harmish@laas.fr>
2016-01-04 14:11:10 +0100

    flexible installation

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-ln -s bashrc $HOME/.bashrc
-ln -s scripts/env $HOME/.env
-ln -s gitconfig $HOME/.gitconfig
-ln -s gitignore_global $HOME/.gitignore_global
-ln -s i3 $HOME/.i3
-ln -s ssh_config $HOME/.ssh/config
-ln -s tmux.conf $HOME/.tmux.conf
-ln -s vimrc $HOME/.vimrc
-ln -s zshrc $HOME/.zshrc
-sed -i "4s?.*?export SCRITPS_INSTALL=`pwd`\/scripts?" scripts/env
+FILE=`readlink -f $0`
+DIR=`dirname $FILE`
+
+ln -s $DIR/bashrc $HOME/.bashrc
+ln -s $DIR/scripts/env $HOME/.env
+ln -s $DIR/gitconfig $HOME/.gitconfig
+ln -s $DIR/gitignore_global $HOME/.gitignore_global
+ln -s $DIR/i3 $HOME/.i3
+ln -s $DIR/ssh_config $HOME/.ssh/config
+ln -s $DIR/tmux.conf $HOME/.tmux.conf
+ln -s $DIR/vimrc $HOME/.vimrc
+ln -s $DIR/zshrc $HOME/.zshrc
+sed -i "4s?.*?export SCRITPS_INSTALL=$DIR\/scripts?" $DIR/scripts/env
