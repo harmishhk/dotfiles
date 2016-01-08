37943c80f2e892c78956f9f702a0cb5c136cc830
Harmish Khambhaita <harmish@laas.fr>
2016-01-08 19:08:18 +0100

    updating i3 config with new programs and fixing install script

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-FILE=`readlink -f $0`
-DIR=`dirname $FILE`
+DIR="$(dirname "$(readlink -f ${BASH_SOURCE[0]})")"
 
 ln -s -f $DIR/bashrc $HOME/.bashrc
 ln -s -f $DIR/scripts/env $HOME/.env
@@ -13,4 +12,4 @@ ln -s -f $DIR/ssh_config $HOME/.ssh/config
 ln -s -f $DIR/tmux.conf $HOME/.tmux.conf
 ln -s -f $DIR/vimrc $HOME/.vimrc
 ln -s -f $DIR/zshrc $HOME/.zshrc
-sed -i "4s?.*?export SCRITPS_INSTALL=$DIR\/scripts?" $DIR/scripts/env || echo "cannot sed $DIR/scripts/env"
+sed -i "4s?.*?export SCRITPS_INSTALL=$DIR\/scripts?" $DIR/scripts/env
