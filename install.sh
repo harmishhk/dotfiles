b36aa10ee1d09032ee49ba151e2cbf48b1b6c5e5
harmish khambhaita <harmish@laas.fr>
2017-04-11 19:23:37 +0200

    creating docker-based-executables for latex commands instead of using functions, to be able to run with node-spawn

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 DIR="$(dirname "$(readlink -f ${BASH_SOURCE[0]})")"
 
+# create symbolic links for dotfiles
 ln -s -f $DIR/bashrc $HOME/.bashrc
 ln -s -f $DIR/scripts/env $HOME/.env
 ln -s -f $DIR/gitconfig $HOME/.gitconfig
@@ -12,4 +13,11 @@ ln -s -f $DIR/ssh_config $HOME/.ssh/config
 ln -s -f $DIR/tmux.conf $HOME/.tmux.conf
 ln -s -f $DIR/vimrc $HOME/.vimrc
 ln -s -f $DIR/zshrc $HOME/.zshrc
+
+# create symbolic links for all execs
+test -d "$HOME/.local/bin" || mkdir -p "$HOME/.local/bin"
+for exec in $DIR/exec/*; do
+    ln -s -f $exec $HOME/.local/bin/$(basename $exec)
+done
+
 sed -i "4s?.*?export SCRITPS_INSTALL=$DIR\/scripts?" $DIR/scripts/env
