1b1992957c73eb63f5bdb1fb2b828bf218921732
Harmish Khambhaita <hkhambhaita@anybotics.com>
2019-04-26 09:44:04 +0200

    Adding small comment

--- install.sh
+++ install.sh
@@ -22,6 +22,7 @@ done
 
 sed -i "4s?.*?export SCRITPS_INSTALL=$DIR\/scripts?" $DIR/scripts/env
 
+# put additional git config only on robots
 if [[ $(hostname -s) = anymal* ]]; then
     ln -s -f $DIR/gitconfig_robot $HOME/.gitconfig_robot
 fi
