613a1305178c931ffb248abef9506d961601bdff
Harmish Khambhaita <harmish@ifi.uzh.ch>
2024-03-05 16:32:51 +0100

    Updating install

--- install.sh
+++ install.sh
@@ -7,10 +7,8 @@ ln -s -f $DIR/bashrc $HOME/.bashrc
 ln -s -f $DIR/scripts/env $HOME/.env
 ln -s -f $DIR/gitconfig $HOME/.gitconfig
 ln -s -f $DIR/gitignore_global $HOME/.gitignore_global
-ln -s -f $DIR/i3 $HOME/.i3
 test -d "$HOME/.ssh" || mkdir "$HOME/.ssh"
 ln -s -f $DIR/ssh_config $HOME/.ssh/config
-ln -s -f $DIR/terminator_config $HOME/.config/terminator/config
 ln -s -f $DIR/tmux.conf $HOME/.tmux.conf
 ln -s -f $DIR/vimrc $HOME/.vimrc
 echo | echo | vim +PluginInstall +qall &>/dev/null
