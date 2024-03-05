6e6b63648e04f6995e9e97c0c95e73c1dfb4fc02
Harmish Khambhaita <harmish@ifi.uzh.ch>
2024-03-05 16:33:15 +0100

    Updating uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -4,9 +4,7 @@ rm $HOME/.bashrc
 rm $HOME/.env
 rm $HOME/.gitconfig
 rm $HOME/.gitignore_global
-rm -rf $HOME/.i3
 rm $HOME/.ssh/config
-rm $HOME/.config/terminator/config
 rm $HOME/.tmux.conf
 rm $HOME/.vimrc
 rm $HOME/.zshrc
